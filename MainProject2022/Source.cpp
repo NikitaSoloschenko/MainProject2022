c248f80eae0d1a360ff862c14295fd4cf0e5360e
Никита <@DESKTOP-UOBOEUK>
2022-09-10 12:02:20 +0300

    Добавьте файлы проекта.

---
+++ MainProject2022/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() // точка входа 
+{
+	setlocale(LC_ALL, "Rus"); // функция, отвечающая за вывод русских синволов
+	cout << "Привет!" << endl; // вывод в консоль
+	
+	return 0;
+}
+  
